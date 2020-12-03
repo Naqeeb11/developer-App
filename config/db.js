314a37f9822108e9b48eded4f21121ff8b3f1f7e
Naqeeb <naqeeburrahman0@gmail.com>
2020-12-03 15:08:29 +0530

    user gravatar encrypt def added and instance save in Atlas

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const db = config.get('mongoURI');
 
 const connectDB = async() => {
     try {
-        await mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+        await mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
         console.log('MongoDB Connected...');
     } catch (err) {
         console.error(err.message);
