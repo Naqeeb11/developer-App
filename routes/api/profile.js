542a2d42932db2e355bbb1c4f2f0052251d88cde
Naqeeb <naqeeburrahman0@gmail.com>
2020-12-04 21:45:37 +0530

    Get/profile/api/me created

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator/check');
 
-// @route     GET api/Profile
-// @des       Test route
-// @access    Public
-router.get('/', (req, res) => res.send('Profile route'));
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
+// @route     GET api/profile/me
+// @desc      Get current users profile
+// @access    Private
+router.get('/me', auth, async(req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']);
+
+        if (!profile) {
+            return res.status(400).json({ msg: 'There is no profile for this user' });
+        }
+
+        res.json(profile);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route     Post api/profile
+// @desc      Create or update users profile
+// @access    Private
+router.post('/', [auth, ], (req, res) => {
+
+})
 module.exports = router;
