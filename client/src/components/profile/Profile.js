349ec4551a658d2418dcadb9c3dfe98f0f6e92b7
Naqeeb <naqeeburrahman0@gmail.com>
2021-04-05 14:03:58 +0530

    ok

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -25,7 +25,7 @@ const Profile = ({
              <Link to='/profiles' className='btn btn-light'>
                  Back To Profiles
              </Link>
-             {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (<Link to='edit-profile' className='btn btn-dark'>
+             {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (<Link to='/edit-profile' className='btn btn-dark'>
                  Edit Profile
              </Link>)}
       <div class="profile-grid my-1">
